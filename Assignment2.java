bc3becda6643e1a6e3922d7333eee1e3e14e6b3e
Talha Safa Küçük <talhasafakucuk@gmail.com>
2024-09-25 15:16:39 +0300

    Created createRandomArray()

--- Assignment2.java
+++ Assignment2.java
@@ -1,4 +1,17 @@
+import java.util.Random;
+
 public class Assignment2
 {
-    
+    public static int[] createRandomArray(int size)
+    {
+        Random ran = new Random(); 
+        int[] newArray = new int[size];
+
+        for(int i = 0 ; i < newArray.length ; i++)
+        {
+            newArray[i] = ran.nextInt(0,101);
+        }
+        
+        return newArray;
+    }
 }
