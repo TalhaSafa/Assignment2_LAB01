e0dce427afe8a2049a56d33650d55dacbdc80a43
Ahmet Oğuz Azık <159693697+ahmetoguzazik2005@users.noreply.github.com>
2024-09-25 16:03:08 +0300

    initialized the array

--- Assignment2.java
+++ Assignment2.java
@@ -59,7 +59,7 @@ public class Assignment2
     }
     // Method to find the differences between the average of the array with every element of the array.
     public double[] AverageDifference(Double average, int[] array ){
-    double[] averageDifference;
+    double[] averageDifference= new double[array.length];
     double diference = 0;
     for ( int i = 0; i < array.length; i++ ){
             diference = array[i] * 1.0 - average; // Casting the integer array to double
