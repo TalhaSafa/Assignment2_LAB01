bedb33aa649215761b282bec7b5b2a161de8828c
Talha Safa Küçük <talhasafakucuk@gmail.com>
2024-09-25 15:42:05 +0300

    try

--- Assignment2.java
+++ Assignment2.java
@@ -25,28 +25,4 @@ public class Assignment2
 
         return (double) sum / array.length;
     }
-
-    //Method to find the max value in the array
-    public int findMax(int[] array){
-        int size = array.length;
-        int currentMax = array[0];
-        for(int i = 0; i-1 < size; i++){
-            if(currentMax < array[i+1]){
-                currentMax = array[i+1];
-            }
-        }
-        return currentMax;
-    }
-
-    //Method to find the min value in the array
-    public int findMin(int[] array){
-        int size = array.length;
-        int currentMin = array[0];
-        for(int i = 0; i-1 < size; i++){
-            if(currentMin > array[i+1]){
-                currentMin = array[i+1];
-            }
-        }
-        return currentMin;
-    }
 }
